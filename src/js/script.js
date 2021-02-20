4bf9f41447eb435dde4cb000590f6d91859313f4
Sultan <caramel@gmail.com>
2021-02-20 23:12:52 +0600

    tabs and acc

--- src/js/script.js
+++ src/js/script.js
@@ -9,10 +9,17 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
     });
-    
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+        $(blockId).slideToggle();
+        $this.toggleClass("active");
     });
 });
